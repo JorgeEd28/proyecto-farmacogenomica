416f18bae15cb916a9de2e82123896ecb8ab6398
Sergio Villicaña <svillicana@grupoabraxas.com>
2018-06-26 22:48:02 -0500

    Read raw data

---
+++ benchmark_preprocess/1_load_raw.R
@@ -0,0 +1,44 @@
+library(Biobase)
+library(crlmm)
+library(dplyr)
+library(purrr)
+library(reshape2)
+
+# Specify directories ---------------------------------------------------------
+
+# Input directory
+datadir <- "BENCHMARK"
+
+# Output directory
+outdir <- "R_Benchmark"
+ldPath(outdir)
+dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+
+# Reading data ----------------------------------------------------------------
+
+# Read in sample annotation info
+samplesheet <- read.csv(file.path(datadir, "benchmark_samplesheet.csv"), 
+                        skip = 6, as.is = TRUE)
+arrayNames <- file.path(datadir, "intensity_data", 
+                        samplesheet[["SentrixBarcode_A"]],
+                        paste(samplesheet[["SentrixBarcode_A"]], 
+                              samplesheet[["SentrixPosition_A"]], 
+                              sep = "_"))
+arrayInfo <- list(barcode = "SentrixBarcode_A", position="SentrixPosition_A")
+
+# Read raw .idats using sampleSheet information
+raw_cnSet <- readIdatFiles(sampleSheet = samplesheet, arrayNames = arrayNames,
+                           arrayInfoColNames = arrayInfo, saveDate = TRUE)
+
+# Generate data frame with expression data set
+batch <- as.Date(protocolData(raw_cnSet)[["ScanDate"]], 
+                 "%m/%d/%Y %H:%M:%S %p") %>% factor()
+levels(batch) <- 1:length(levels(batch))
+dict <- data.frame(variable = sampleNames(protocolData(raw_cnSet)), batch)
+raw_eset <- assayData(raw_cnSet) %>% as.list %>% map(as.data.frame) %>% map(melt)
+raw_eset_batch <- map2(raw_eset, list(dict), left_join) %>% 
+  map(.f = arrange, batch, variable)
+
+# Save RDS --------------------------------------------------------------------
+
+saveRDS(raw_eset_batch, file.path(outdir, "raw_eset.rds"))
