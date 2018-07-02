c60e2e3a2ef0e60b300e44681703e9dde1c8192e
Sergio Villicaña <svillicana@grupoabraxas.com>
2018-07-02 00:12:21 -0500

    Create annotation file

---
+++ benchmark_preprocess/0_manifest.R
@@ -0,0 +1,80 @@
+library(biomaRt)
+library(dplyr)
+library(stringr)
+library(IRanges)
+library(GenomicRanges)
+
+# Specify directories ---------------------------------------------------------
+
+# Input directory
+datadir <- "BENCHMARK/Manifest"
+
+# Output directory
+outdir <- "R_Benchmark"
+
+# Read manifest file ----------------------------------------------------------
+
+manifest_raw <- read.csv(file.path(datadir, "CODIGO46_2017_01_20011739X343381_B1.csv"), 
+                     as.is = TRUE, skip = 7)
+
+# Clean snp name and select columns
+manifest_renames <- manifest_raw %>% 
+  select(IlmnID, Name, Chr, MapInfo, Source, SourceStrand) %>%
+  mutate(Variant = sub(".*(rs[0-9]*).*", "\\1", Name))
+
+# Add missing chromosomes
+add_chr <- manifest_renames %>% 
+  filter(Chr == 0, grepl(':', Variant)) %>% 
+  mutate(Position = sub("(GSA-)*([0-9]*:[0-9]*).*", "\\2", Variant) %>% strsplit(":"),
+         Chr = Position[[1]][1], MapInfo = strtoi(Position[[1]][2])) %>%
+  select(-Position)
+  
+manifest_clean <- manifest_renames %>% anti_join(add_chr, by = "IlmnID") %>%
+  bind_rows(add_chr)
+
+# Select databases ------------------------------------------------------------
+
+# SNP db
+snp_db <- useMart("ENSEMBL_MART_SNP", dataset="hsapiens_snp", host="grch37.ensembl.org")
+# Ensembl gene db
+ensembl_db <- useMart("ensembl", dataset="hsapiens_gene_ensembl", host="grch37.ensembl.org")
+
+# Get gene names --------------------------------------------------------------
+
+# By SNP name
+genes_by_snp_ensembl <- getBM(attributes = c("refsnp_id", "ensembl_gene_stable_id"),
+                           filters = c("snp_filter"),
+                           values = manifest_clean %>% filter(str_detect(Variant, "rs*")) %>% select(Variant),
+                           mart = snp_db) %>%
+  filter(ensembl_gene_stable_id != "")
+genes_by_snp_hgnc <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"),
+                           filters = c("ensembl_gene_id"),
+                           values = select(genes_by_snp_ensembl, ensembl_gene_stable_id),
+                           mart = ensembl_db)
+genes_by_snp <- genes_by_snp_ensembl %>%
+  left_join(genes_by_snp_hgnc, by = c("ensembl_gene_stable_id" = "ensembl_gene_id")) %>%
+  transmute(Variant = refsnp_id, Ensembl = ensembl_gene_stable_id, Gene = hgnc_symbol)
+
+# By position
+all_genes <- getBM(attributes = c("chromosome_name","start_position","end_position","ensembl_gene_id","hgnc_symbol"),
+                   filters = c("chromosome_name"), values = list(c(1:22,"X","Y")), mart = ensembl_db)
+all_genes_rd <- with(all_genes, GRanges(seqnames = chromosome_name, IRanges(start_position, end_position)))
+
+filter_var <- manifest_clean %>% anti_join(genes_by_snp)
+filter_var_rd <- with(filter_var, GRanges(seqnames = Chr, IRanges(MapInfo, MapInfo)))
+
+var_overlaps <- findOverlaps(filter_var_rd, all_genes_rd)
+
+genes_by_pos <- data.frame(Variant = filter_var[queryHits(var_overlaps), "Variant"], 
+                           all_genes[subjectHits(var_overlaps), c("ensembl_gene_id", "hgnc_symbol")] %>% 
+                             rename(Ensembl = ensembl_gene_id, Gene = hgnc_symbol))
+
+# Full merge
+
+genes_by_snp_pos <- rbind(genes_by_snp, genes_by_pos)
+
+anno_df <- left_join(manifest_clean, genes_by_snp_pos)
+
+# Save annotation file
+
+write.csv(anno_df, quote = FALSE, na = "")
--- benchmark_preprocess/preprocess.R
+++ benchmark_preprocess/preprocess.R
@@ -1,10 +1,9 @@
 library(Biobase)
 library(crlmm)
-library(hapmap370k)
 library(dplyr)
-library(ggplot2)
 library(purrr)
 library(reshape2)
+library(ff)
 
 # Specify directories ---------------------------------------------------------
 
@@ -12,9 +11,12 @@ library(reshape2)
 datadir <- "BENCHMARK"
 
 # Output directory
-outdir <- paste("R_Benchmark/")
+outdir <- "R_Benchmark"
 ldPath(outdir)
 dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+
+# ff options
+options(ffcaching="ffeachflush")
 
 # Reading data ----------------------------------------------------------------
 
@@ -28,43 +30,9 @@ arrayNames <- file.path(datadir, "intensity_data",
                               sep = "_"))
 arrayInfo <- list(barcode = "SentrixBarcode_A", position="SentrixPosition_A")
 
-# Read raw .idats using sampleSheet information
-raw_cnSet <- readIdatFiles(sampleSheet = samplesheet, arrayNames = arrayNames,
-                           arrayInfoColNames = arrayInfo, saveDate = TRUE)
-
-# Generate data frame with expression data set
-batch <- as.Date(protocolData(raw_cnSet)[["ScanDate"]], 
-                        "%m/%d/%Y %H:%M:%S %p") %>% factor()
-levels(batch) <- 1:length(levels(batch))
-dict <- data.frame(variable = sampleNames(protocolData(raw_cnSet)), batch)
-raw_eset <- assayData(raw_cnSet) %>% as.list %>% map(as.data.frame) %>% map(melt)
-raw_eset_batch <- map2(raw_eset, list(dict), left_join) %>% 
-  map(.f = arrange, batch, variable)
-
-# Plot raw data ---------------------------------------------------------------
-
-# Red channel
-ggplot(raw_eset_batch[["R"]], aes(x = variable, y = log2(value), fill = batch)) + 
-  geom_boxplot(alpha=0.5) + scale_fill_brewer(palette = "Dark2") +
-  theme(axis.text.x = element_text(angle = 90)) + xlab("Microarreglo") +
-  labs(x = "Microarreglo", y = expression(paste("log"[2], "(intensidad)")))
-
-# Green channel
-ggplot(raw_eset_batch[["G"]], aes(x = variable, y = log2(value), fill = batch)) + 
-  geom_boxplot(alpha=0.5) + scale_fill_brewer(palette = "Dark2") +
-  theme(axis.text.x = element_text(angle = 90)) + xlab("Microarreglo") +
-  labs(x = "Microarreglo", y = expression(paste("log"[2], "(intensidad)")))
-
-# Preprocessing and genotyping ------------------------------------------------
-
-# Load package ff
-library(ff)
-options(ffcaching="ffeachflush")
-
 # Normalized data
-cnSet <- genotype.Illumina(sampleSheet = samplesheet, arrayNames = arrayNames,
-                           arrayInfoColNames = arrayInfo, cdfName = "human370v1c",
-                           batch = batch)
+cnSet <- genotype.Illumina(sampleSheet = samplesheet, arrayNames = arrayNames, 
+                           cdfName = "nopackage", batch = batch)
 
 # Plot normalized data --------------------------------------------------------
 plot(crlmmResult[["SNR"]][,], pch=as.numeric(scanbatch), xlab="Array", ylab="SNR",
