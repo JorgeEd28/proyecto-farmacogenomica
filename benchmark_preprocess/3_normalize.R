552eb8a8271d1a46da9b9ae1ba435894093c42df
Sergio Villicaña <svillicana@grupoabraxas.com>
2018-07-04 10:27:37 -0500

    Normalize data

---
+++ benchmark_preprocess/3_normalize.R
@@ -0,0 +1,53 @@
+library(Biobase)
+library(crlmm)
+library(dplyr)
+library(purrr)
+library(reshape2)
+library(ff)
+
+# Specify directories ---------------------------------------------------------
+
+# Input directory
+datadir <- "BENCHMARK"
+
+# Output directory
+outdir <- "R_Benchmark"
+
+# ff options
+options(ffcaching="ffeachflush")
+setwd(outdir)
+
+# Reading data ----------------------------------------------------------------
+
+# Read in sample annotation info
+samplesheet <- read.csv(file.path(datadir, "benchmark_samplesheet.csv"), 
+                        skip = 6, as.is = TRUE)
+manifest_raw <- read.csv(file.path(datadir, "CODIGO46_2017_01_20011739X343381_B1.csv"), 
+                         as.is = TRUE, skip = 7)
+batch <- read.csv(file.path(datadir, "sample-batch.csv"), 
+                  as.is = TRUE, colClasses = c("character", "factor"))
+arrayNames <- file.path(datadir, "intensity_data", 
+                        samplesheet[["SentrixBarcode_A"]],
+                        paste(samplesheet[["SentrixBarcode_A"]], 
+                              samplesheet[["SentrixPosition_A"]], 
+                              sep = "_"))
+
+# Annotation data frame -------------------------------------------------------
+
+anno_file <- manifest_raw %>%
+  cbind(isSnp = TRUE) %>%
+  rename(featureNames = Name, position = MapInfo, chromosome = Chr)
+
+# Normalized data -------------------------------------------------------------
+
+cnSet <- genotype.Illumina(sampleSheet = samplesheet, arrayNames = arrayNames, 
+                           call.method="krlmm", cdfName = "nopackage", 
+                           anno = anno_file, genome = "hg19", batch = batch, 
+                           quantile.method="between", nopackage.norm="quantile")
+
+# Save RDS --------------------------------------------------------------------
+
+saveRDS(cnSet, "normalize_data.rds")
+
+
+
