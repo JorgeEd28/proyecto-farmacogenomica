3fae57d1ee354f7ddee7e34b22739329528372d0
Sergio Villicaña <svillicana@grupoabraxas.com>
2018-07-03 19:57:25 -0500

    Create comparison of PED files

---
+++ benchmark_preprocess/3_compare_ped.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(ggplot2)
+
+# Specify directories ---------------------------------------------------------
+
+# Input directory
+datadir <- "GenomeStudio"
+
+# Output directory
+outdir <- "R_Benchmark"
+
+# Load PEDs -------------------------------------------------------------------
+
+read.table()
