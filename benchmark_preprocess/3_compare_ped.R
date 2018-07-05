18cdc3a768919d53d7c9ac7e9c02c40fff785a8c
Sergio Villicaña <svillicana@grupoabraxas.com>
2018-07-05 00:51:27 -0500

    Generate table of comparitions and proportions; create bar plot and density

--- benchmark_preprocess/3_compare_ped.R
+++ benchmark_preprocess/3_compare_ped.R
@@ -23,7 +23,6 @@ ped_2 <- read.table(file.path(datadir, "MAPC46.PED"),
 # Compare MAPs ----------------------------------------------------------------
 
 map_identical <- identical(map_1[2], map_2[2])
-map_identical
 if (!map_identical) {stop("Los MAP no son idénticos, no se pueden comparar PED")
 }
 
@@ -38,7 +37,41 @@ ped_2_t <- as.data.frame(t(ped_2), stringsAsFactors = FALSE)
 colnames(ped_2_t) <- ped_2_t[2,]
 ped_2_t <- ped_2_t[-c(1:6),]
 
-not_equal_snp <- data.frame()
+# Compare
+not_equal_snp <- data.frame(row.names = 1:nrow(ped_1_t))
 for(i in names(ped_1_t)){
   not_equal_snp[[i]] <- ifelse(ped_1_t[[i]] == ped_2_t[[i]], 0, 1)
 }
+
+# Analysis by sample ----------------------------------------------------------
+
+prop_not_equal_snp <- data.frame(muestra = colnames(not_equal_snp),
+                                 proporcion = colMeans(not_equal_snp), 
+                                 row.names = NULL) 
+
+# Plots -----------------------------------------------------------------------
+
+# Bar plot
+prop_bar <- ggplot(prop_not_equal_snp, aes(x = reorder(muestra, -proporcion), y = proporcion, fill = proporcion)) +
+  geom_bar(stat = "identity", show.legend=F) + theme_minimal() + 
+  labs(x = "Microarreglo", y = "Proporción")
+
+# Density plot
+prop_dens <- ggplot(prop_not_equal_snp, aes(proporcion, color = "blue", fill="blue")) +
+  geom_density(alpha = 0.5, show.legend=F) +
+  labs(x = "Microarreglo", y = "Densidad")
+
+# Save RDS --------------------------------------------------------------------
+
+# Save PNG
+png(file.path(outdir, "prop_not_equal_snp_barplot.png"), width = 2400, height = 1200, res = 300)
+print(prop_bar)
+dev.off()
+
+png(file.path(outdir, "prop_not_equal_snp_density.png"), width = 2400, height = 1200, res = 300)
+print(prop_dens)
+dev.off()
+
+# Save RDS and CSV
+saveRDS(prop_bar, file.path(outdir, "prop_not_equal_snp_barplot.rds"))
+saveRDS(prop_dens, file.path(outdir, "prop_not_equal_snp_density.rds"))
