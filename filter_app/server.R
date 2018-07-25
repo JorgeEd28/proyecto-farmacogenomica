cbec3d861bc5ac97ea1d9522e21b8d3af9e96d11
Sergio Villicaña <svillicana@grupoabraxas.com>
2018-07-25 15:54:01 -0500

    Upload filter file

--- filter_app/server.R
+++ filter_app/server.R
@@ -1,10 +1,19 @@
+library(dplyr)
+
+options(shiny.maxRequestSize=150*1024^2)
+
 function(input, output) {
+  # Initialize reactive variables
+  v <- reactiveValues(filter_list = NULL, anno_probes = NULL)
+  
+  # Annotation data -----------------------------------------------------------
+  
   # Display data
   zz <- gzfile("annotation_file.csv.gz", "rt")
-  data <- read.csv(zz, colClasses = c("character", "character", "factor", 
+  anno <- read.csv(zz, colClasses = c("character", "character", "factor", 
                                       "integer", "character", "character"))
   output$annotation <- DT::renderDataTable(DT::datatable(
-    data,
+    anno,
     colnames = c(
       "Probe name",
       "Probe-variant name",
@@ -29,11 +38,59 @@ function(input, output) {
     downloadHandler(
       filename = "annotation_file_filter.csv",
       content = function(file) {
-        write.csv(data[input[["annotation_rows_all"]],],
-                  file,
-                  row.names = FALSE,
-                  quote = FALSE,
-                  na = "")
+        write.csv(anno[input[["annotation_rows_all"]],], file, 
+                  row.names = FALSE, quote = FALSE, na = "")
       }
     )
+  
+  # Upload filter data --------------------------------------------------------
+  
+  # Output table
+  output$contents <- renderTable({
+    
+    # Ensure that values are available
+    req(input$filter)
+    
+    # Read lines of file
+    tryCatch(
+      {
+        v$filter_list <- readLines(input$filter$datapath)
+      },
+      error = function(e) {
+        # return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
+    
+    # Activate comparison when click in button
+    v$anno_probes <- anno %>% filter((!!sym(input$filter_type)) %in% v$filter_list)
+    
+    # Return summary table
+    summary_table <- v$anno_probes %>% select(Variant, Ensembl, Gene) %>%
+      group_by(Gene) %>% 
+      summarize(Ensembl = paste(unique(Ensembl), collapse = ", "), 
+                Variants = n_distinct(Variant)) %>%
+      ungroup()
+    return(summary_table)
+  })
+  
+  # Return not matching filters
+  output$not_found <- renderText({ 
+    if(is.null(v$anno_probes)){
+      return("Upload a filter file to start")
+    }
+    not_found <- setdiff(v$filter_list,
+                         v$anno_probes[[input$filter_type]])
+    return(paste("Following", input$filter_type, 
+                "IDs were not found in annotation dataset:",
+                paste(not_found, collapse = ', ')))
+    
+    # return(paste("Following", input$filter_type,
+    #              "IDs were not found in annotation dataset:",
+    #              not_found))
+  })
 }
+
+# Read MAP
+# probes <- read.table(input$map$datapath, sep = "\t", 
+#                      colClasses = c("NULL", "character", "NULL", "NULL"))
--- filter_app/ui.R
+++ filter_app/ui.R
@@ -2,40 +2,56 @@ navbarPage(
   "Filter PEDs App",
   
   # View and filter annotation ------------------------------------------------
-  tabPanel(
-    "Annotation",
-    # Display name
-    titlePanel("Annotation dataset"),
-    
-    # Display table
-    DT::dataTableOutput("annotation"),
-    
-    # Download button
-    downloadButton(outputId = "download_filtered",
-                   label = "Download Filtered Data")
+  tabPanel("Annotation",
+           
+           # Display name
+           titlePanel("Annotation dataset"),
+           
+           # Display table
+           DT::dataTableOutput("annotation"),
+           
+           # Download button
+           downloadButton(outputId = "download_filtered",
+                          label = "Download Filtered Data")
   ),
   
-  # Upload MAP, PED & filter --------------------------------------------------
-  tabPanel(
-    "Upload files",
-    # Display name
-    titlePanel("Uploading Files"),
+  # Upload filter data --------------------------------------------------------
+  tabPanel("Select filters",
+           
+           # Display name
+           titlePanel("Select filters"),
+           
+           sidebarLayout(
+             sidebarPanel(
+               # Input filter
+               fileInput("filter", "Choose a txt/csv filter file (one value per line)",
+                         multiple = FALSE, accept = c(
+                           "text/comma-separated-values,text/plain")),
+               
+               # Select filter to apply
+               radioButtons("filter_type", "Choose a filter",
+                            choices = list("Variant name" = "Variant",
+                                           "Ensembl ID" = "Ensembl",
+                                           "Gene symbol" = "Gene"),
+                            selected = "Variant")
+             ),
+             
+             mainPanel(
+               h4(paste("Summary table of features")),
+               tableOutput("contents"),
+               textOutput("not_found")
+             )
+           )
+  ),
+  
+  # Upload MAP ----------------------------------------------------------------
+  tabPanel("Select MAP"
     
-    # Input MAP
-    fileInput( "map", "Choose a MAP File", multiple = FALSE, accept = "tsv"),
-    
-    # Input MAP
-    fileInput("ped", "Choose a PED File", multiple = FALSE, accept = "tsv"),
-    
-    # Input filter
-    fileInput("filter", "Choose a filter file (values separated by commas)",
-              multiple = FALSE, accept = "csv"),
-    
-    # Select filter to apply
-    radioButtons("radio", "Choose a filter",
-                 choices = list("Variant name" = 1,
-                                "Ensembl ID" = 2,
-                                "Gene symbol" = 3),
-                 selected = 1)
   )
 )
+
+# Input MAP
+#fileInput("map", "Choose a MAP File", multiple = FALSE, accept = "tsv"),
+
+# Input MAP
+#fileInput("ped", "Choose a PED File", multiple = FALSE, accept = "tsv"),
