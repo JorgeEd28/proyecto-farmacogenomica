cfeb9b8f15ab32f7b99e63d893c4639db14741d0
Sergio Villicaña <svillicana@grupoabraxas.com>
2018-08-09 01:10:11 -0500

    Compile PDF report from RNW

---
+++ benchmark_preprocess/compile_report.R
@@ -0,0 +1,18 @@
+# Create report
+output_sufix <- "Reporte_benchmark"
+file_name <- paste(output_sufix, "tex", sep = ".")
+
+# Compile Rnw to Latex
+knitr::knit("Microarray.Rnw", output = file_name)
+
+# Compile Latex to PDF
+system(paste("pdflatex -interaction=nonstopmode", file_name))
+# Add bibliography
+system(paste("bibtex", output_sufix))
+# Add figures names
+system(paste("pdflatex -interaction=nonstopmode", file_name))
+
+# Delete not needed files
+files <- list.files(pattern = output_sufix)
+files <- files[!grepl(".pdf", files)]
+files <- files[!grepl(".tex", files)]
